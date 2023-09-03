d3b9f5cdc40e140ea1dc03a8fe4fccec49b6797f
Craig-Hudson <>
2023-09-03 23:16:46 +0000

    retrive stock numbers from worksheey

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -60,8 +61,27 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("sales worksheet updated successfully\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock, and calculate surplus for each type
 
+    The surplus is defined as the sales figure subtracted by the stock
+    Positive indicates waste
+    Negative indicates more sandwiches made on the day
+    """
+    print("Calculating surplus data.. \n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    pprint(stock)
+    stock_row = stock[-1]
+    print(stock_row)
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def main():
+    """
+    run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+print("Welcome to love sandwiches")
+main()
